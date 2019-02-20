7c53b9cd3ddd560120ad95b47116ff8fda915833
Guilherme Farias <guilherme@guiky.com.br>
2019-02-20 18:06:51 -0300

    Form working

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,71 @@ function init() {
 		window.scrollTo(0, document.querySelector('#' + id).offsetTop - 50);
 	}
 
+	function submitContact(e) {
+		e.preventDefault();
+
+		var formElement = document.querySelector('.contact-form');
+		var formData = '';
+
+		formData += 'name=' + encodeURIComponent(formElement.elements.name.value) + '&';
+		formData += 'email=' + encodeURIComponent(formElement.elements.email.value) + '&';
+		formData += 'subject=' + encodeURIComponent(formElement.elements.subject.value) + '&';
+		formData += 'message=' + encodeURIComponent(formElement.elements.message.value);
+
+		sendEmail('mail-digitalbits-contato', formData, formElement, function() {
+			formElement.elements.name.value = '';
+			formElement.elements.email.value = '';
+			formElement.elements.subject.value = '';
+			formElement.elements.message.value = '';
+		});
+	};
+
+	function submitCarrer(e) {
+		e.preventDefault();
+
+		var formElement = document.querySelector('.modal-form');
+		var formData = '';
+
+		formData += 'name=' + encodeURIComponent(formElement.elements.name.value) + '&';
+		formData += 'email=' + encodeURIComponent(formElement.elements.email.value) + '&';
+		formData += 'subject=' + document.querySelector('.modal-title').innerText + '&';
+		formData += 'message=' + encodeURIComponent(formElement.elements.message.value);
+
+		sendEmail('mail-digitalbits-carrer', formData, formElement, function() {
+			formElement.elements.name.value = '';
+			formElement.elements.email.value = '';
+			formElement.elements.message.value = '';
+		});
+	};
+
+	function sendEmail(type, formData, formElement, resetForm) {
+		var submitRequest = new XMLHttpRequest();
+		var respElement = document.createElement('div');
+
+		submitRequest.open('POST','https://guilhermefarias.com.br/' + type,true);
+		submitRequest.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+		submitRequest.onreadystatechange = function(){
+			if (submitRequest.readyState === 4 && submitRequest.status === 200 && submitRequest.responseText === 'OK'){
+				respElement.setAttribute('class','resp');
+				respElement.innerHTML = 'Mensagem enviada com sucesso!'; // Message sent successfully!
+				formElement.appendChild(respElement);
+				resetForm();
+				setTimeout(function(){
+					formElement.removeChild(respElement);
+				},5000);
+			} else if(submitRequest.readyState === 4){
+				respElement.setAttribute('class','resp error');
+				respElement.innerHTML = 'Houve um erro ao enviar a mensagem'; // There was an error sending the message
+				formElement.appendChild(respElement);
+				setTimeout(function(){
+					formElement.removeChild(respElement);
+				},5000);
+			}
+		};
+
+		submitRequest.send(formData);
+	};
+
 	function scollCheck() {
 		if (window.pageYOffset === lastCheck) {
 			return;
@@ -53,6 +118,15 @@ function init() {
 	window.goTo = goTo;
 	window.openModal = openModal;
 	window.closeModal = closeModal;
+	document.querySelector('.modal-form').addEventListener('submit', submitCarrer);
+	document.querySelector('.contact-form').addEventListener('submit', submitContact);
+	document.querySelector('.modal-close').addEventListener('click', closeModal);
+	document.addEventListener('keyup', function(e) {
+		if (e.keyCode === 27) {
+			closeModal();
+		}
+	});
+
 	setInterval(scollCheck, 200);
 	scollCheck();
 }
